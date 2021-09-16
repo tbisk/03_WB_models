87414e24016364bc68720094a175f87005d3a765
tah_sin <tbisk@web.de>
2021-09-16 14:29:43 +0200

    Neue Tabellen + UNIQUE

---
+++ 01_FirstModel/sql/02_tabs_with_inserts.sql
@@ -0,0 +1,51 @@
+-- Vorbereitungen
+
+DROP DATABASE IF EXISTS mydb;
+CREATE DATABASE IF NOT EXISTS mydb;
+
+-- Detailtabelle: Verbindung zur MT über Fremdschlüssel
+CREATE TABLE IF NOT EXISTS `mydb`.`cats` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `cat_name` VARCHAR(45) NOT NULL,
+  `fur_color` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+-- Struktur MT
+DESCRIBE mydb.cats;
+
+-- Inserts: MT (Maastertabelle)
+INSERT INTO `mydb`.`cats` (`id`, `cat_name`, `fur_color`) VALUES (NULL, "Grizabella", "white");
+INSERT INTO `mydb`.`cats` (`id`, `cat_name`, `fur_color`) VALUES (NULL, "Alonzo", "grey");
+INSERT INTO `mydb`.`cats` (`id`, `cat_name`, `fur_color`) VALUES (NULL, "Mausi", "striped");
+
+-- Inhalte: MT
+SELECT * FROM mydb.cats;
+
+-- Detailtabelle (DT): Verbindung zur MT über Fremdschlüssel
+CREATE TABLE IF NOT EXISTS `mydb`.`servants` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `servant_name` VARCHAR(45) NOT NULL,
+  `yrs_served` INT NOT NULL,
+  `cats_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_servants_cats_idx` (`cats_id` ASC) VISIBLE,
+  UNIQUE INDEX `cats_id_UNIQUE` (`cats_id` ASC) VISIBLE,
+  CONSTRAINT `fk_servants_cats`
+    FOREIGN KEY (`cats_id`)
+    REFERENCES `mydb`.`cats` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+-- Struktur: DT
+DESCRIBE mydb.servants;
+
+-- Inserts: DT (Detailtabelle)
+INSERT INTO `mydb`.`servants` (`id`, `servant_name`, `yrs_served`, `cats_id`) VALUES (DEFAULT, "Peter", 5, 1);
+INSERT INTO `mydb`.`servants` (`id`, `servant_name`, `yrs_served`, `cats_id`) VALUES (DEFAULT, "Fatih", 2, 3);
+-- Das sollte eigentlich nicht gehen! -> UNIQUE
+INSERT INTO `mydb`.`servants` (`id`, `servant_name`, `yrs_served`, `cats_id`) VALUES (DEFAULT, "Holger", 3, 1);
+
+-- Inhalte: DT
+SELECT * FROM mydb.servants;
